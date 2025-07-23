44caacb6fa69f8923839d36e35c811626519b257
runningpoem30 <aryaanandpathak30@gmail.com>
2025-07-23 16:01:48 +0530

    changes

--- client/src/pages/UserVerified.jsx
+++ client/src/pages/UserVerified.jsx
@@ -18,6 +18,5 @@ export default UserVerified
 
 
 
-
 // http://localhost:8000/api/user/verify/688006d6fde5d6df1b4928ab/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODgwMDZkNmZkZTVkNmRmMWI0OTI4YWIiLCJpYXQiOjE3NTMyMjA4MjMsImV4cCI6MTc1MzIyMTcyM30.aivIBqU0rH3S2au9pQbYdyadsrCR-Nn2eotn_tJTN5g
 

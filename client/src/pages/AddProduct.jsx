162f58112f3e0ea007037d64b8e13a084ea1efbb
runningpoem30 <aryaanandpathak30@gmail.com>
2025-07-28 20:20:16 +0530

    changes

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import { baseURL } from '@/BaseUrl';
+import { IoIosArrowDropdown } from "react-icons/io";
 
 function AddProduct() {
   
-  async function fetchCategories(){
+  const [data , setData] = useState();
+  const [showDropDown , setShowDropDown] = useState(false);
+
+
+  function toggleDropDown(){
+      setShowDropDown((prev) => !prev);
+  }
+
+    async function fetchCategories(){
     try {
-      const result = await fetch("")
+      const result = await fetch(`${baseURL}api/get-all-categories`)
+      const fetchData = await result.json()
+      console.log(fetchData.data)
+      setData(fetchData.data)
     }
     catch(err){
       console.log(err)
     }
   }
+  useEffect(() => {
+    fetchCategories()
+  },[])
+
 
 
   return (
     <div>
+
         <span className='text-5xl font-bold text-[#F8CB46]'>Ration</span>
     <span className='text-5xl font-bold text-[#54B226]'>Seva</span>
     <h1>Hi Vendor , Please Add Your Product</h1>
-    <div className="h-[300px] overflow-y-auto p-2 space-y-2 bg-white shadow rounded">
 
-</div>
     <div className='flex flex-col gap-y-[4px] mt-[90px]'>
       <div>
         <input className='border border-gray-300 p-[15px] rounded-2xl bg-gray-100 pr-[100px]' placeholder='name'></input>
@@ -37,6 +53,33 @@ function AddProduct() {
       <div>
         <input className='border border-gray-300 p-[15px] rounded-2xl bg-gray-100 pr-[100px]' placeholder='description'></input>
       </div>
+     <div className="relative inline-block">
+      <button
+        onClick={toggleDropDown}
+        className='flex items-center justify-between font-grey-100 border border-gray-300 py-[12px] px-4 rounded-2xl bg-gray-100 cursor-pointer w-[335px]'
+      >
+        <span>Category</span>
+        <IoIosArrowDropdown className='text-xl' />
+      </button>
+
+      {showDropDown && (
+        <div className="absolute mt-2 space-y-2">
+          {data?.length > 0 &&
+            data.map((item) => (
+              <div
+                key={item._id}
+                className='border border-gray-300 py-[12px] px-4 rounded-2xl bg-gray-100 cursor-pointer w-[335px]'
+              >
+                {item.name}
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+ 
+
+      
+
     </div>
     </div>
     

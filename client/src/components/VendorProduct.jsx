7bc65839166102c6aa689a417e3a3ebf61d3b096
runningpoem30 <aryaanandpathak30@gmail.com>
2025-07-29 20:35:30 +0530

    changes

--- client/src/components/VendorProduct.jsx
+++ client/src/components/VendorProduct.jsx
@@ -7,7 +7,7 @@ function VendorProduct({name, unit , stock , discount , description , category, 
     
   return (
     <div className=''>
-      <div className='bg-black-500 shadow-md rounded-lg p-4 w-[400px] h-[600px]'>
+      <div className='bg-black-500 shadow-md rounded-lg p-4 w-[400px]'>
         <div>
         <h1 className='w-[100px]' className='text-4xl font-bold text-[#54B226]'>{name}</h1>
               </div>
--- client/src/pages/VendorDashboard.jsx
+++ client/src/pages/VendorDashboard.jsx
@@ -25,15 +25,23 @@ function VendorDashboard() {
         getData()
    }, [])
   return (
-    <div className='flex flex-row'>
-      {
-         productData && productData.length > 0 ? <div>
-           {
-            productData.map((item) => <div className='flex'><VendorProduct name={item.name} images={item.image} discount={item.discount} description={item.description} stock={item.stock} unit={item.unit}></VendorProduct></div>)
-           }
-        </div>  : null
-      }
-    </div>
+    <div className='flex flex-wrap gap-4'>
+  {
+    productData && productData.length > 0 && productData.map((item) => (
+      <VendorProduct
+        key={item._id}
+        name={item.name}
+        images={item.image}
+        discount={item.discount}
+        description={item.description}
+        stock={item.stock}
+        unit={item.unit}
+        createdBy={item.createdBy.shopName}
+        category={item.category.name}
+      />
+    ))
+  }
+</div>
   )
 }
 
